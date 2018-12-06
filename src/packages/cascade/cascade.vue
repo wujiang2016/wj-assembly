aeb50efb0bd06bdb346733154ab49b9e940bee0b
wujiang2016 <wujiang8807@163.com>
2018-12-06 16:24:15 +0800

    级联选择器bug更改

--- src/packages/cascade/cascade.vue
+++ src/packages/cascade/cascade.vue
@@ -1,9 +1,8 @@
 <template>
     <ul class="wj-cascade-menu">
-      	<li v-for="(option,index) in options" :key="option.value" @click.stop="handleSelect(option)" @mouseenter="isShow=index;" @mouseleave="isShow=-1;" class="wj-cascade-item" :class="option.children?'wj-cascade-item-extensible':''">
+      	<li v-for="(option,index) in options" :key="option.value" @click.stop="handleSelect(option)" @mouseenter="isShow=index;showCascade($event);" @mouseleave="isShow=-1;" class="wj-cascade-item" :class="option.children?'wj-cascade-item-extensible':''">
       		{{option.label}}
-      		 <!-- -->
-        	<cascade v-show="isShow==index" @input="changeValue" v-if="option.children" :options="option.children"></cascade>
+        	<cascade :style="{top:top,height:height,'overflow-y':scrollY}" v-show="isShow==index" @input="changeValue" v-if="option.children" :options="option.children"></cascade>
     	</li>
     </ul>
 </template>
@@ -23,21 +22,41 @@ export default{
 	},
 	data(){
 		return{
+		    height: 'auto',
+		    scrollY: 'hidden',
+		    top: 0,
       		isShow: -1,
 		}
 	},
 	methods:{
+	    showCascade(e) {
+	      	var h=document.documentElement.clientHeight || document.body.clientHeight
+	      	var distance=h-(e.y-e.offsetY)
+	      	var childrenLen=this.options[this.isShow].children&&this.options[this.isShow].children.length
+	      	if (childrenLen) {
+		      	this.top=Math.min(distance-childrenLen*34,0)+'px'
+		      	if (childrenLen*34>h) {
+		      		this.height=h+'px'
+		      		this.scrollY='scroll'
+			      	this.top=e.offsetY-e.y+'px'
+		      	} else {
+		      		this.height='auto'
+		      		this.scrollY='hidden'
+		      	}
+	      	}
+	    },
 	    handleSelect(opts) {
-	      this.changeValue(opts);
+	      	this.changeValue(opts);
 	    },
 	    changeValue(opts) {
-	      this.$emit('input', opts);
+	      	this.$emit('input', opts);
 	    },
+	},
+	watch:{
 	}
 }
 </script>
 <style>
-	
 @font-face {
   	font-family: 'element-icons';
   	src: url('../fonts/element-icons.woff'),url('../fonts/element-icons.ttf');
@@ -56,8 +75,8 @@ export default{
     margin: 0;
     padding: 0;
     min-width: 160px;
-        position: absolute;
-    left: 100%;top:0;
+    position: absolute;
+    left: 100%;top:0;border: 1px solid #eee;
     .wj-cascade-item {
 	    font-size: 14px;
 	    padding: 8px 20px;
--- src/packages/cascade/wj-cascade.vue
+++ src/packages/cascade/wj-cascade.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wj-select"
     @click.stop="handleToggleMenu()">
-    <span @click.stop="handleToggleMenu()"
+    <span @click.stop="handleToggleMenu();checkPosition($event);"
       :class="{active: isShow}">{{currentOption.label || '请选择'}}
         <i class="caret"></i>
     </span>
@@ -9,7 +9,7 @@
     <input
       v-bind:value="value"
       v-on:input="changeValue">
-    <cascade v-show="isShow" @input="changeValue" :options="options"></cascade>
+    <cascade :style="{top:top+'px',left:left+'px',height:height,'overflow-y':scrollY}" v-show="isShow" @input="changeValue" :options="options"></cascade>
   </div>
 </template>
 
@@ -30,15 +30,53 @@ export default {
   },
   data() {
     return {
+      height: 'auto',
+      scrollY: 'hidden',
       isShow: false,
       currentOption: {},
+      top: 0,
+      left: 0,
+      scrollTop: '',
+      cascadeTimer: '',//当点击一个select，其它的要隐藏
+      e:'',//当前被点击的事件e
     }
   },
   methods: {
-    handleToggleMenu(flag) {
-      this.isShow = flag === false ? false : !this.isShow;
+    handleScroll(e) {
+      if (('UL'==e.target.tagName) && ('wj-cascade-menu'==e.target.className)) {
+        return;
+      }
+      if (this.scrollTop) {
+        this.top-=e.target.scrollTop-this.scrollTop
+      }
+      this.scrollTop=e.target.scrollTop
     },
-    hideMenu() {
+    checkPosition(e) {
+      this.left=e.x-e.offsetX-e.target.offsetLeft
+      this.top=e.y-e.offsetY+20-e.target.offsetTop
+      var h=document.documentElement.clientHeight || document.body.clientHeight
+      var distance=h-(e.y-e.offsetY)
+      var optionLen=this.options&&this.options.length
+      if (optionLen) {
+        this.height='auto'
+        this.scrollY='visible'
+        if (optionLen*34>h) {
+          this.height=h-10+'px'
+          this.scrollY='scroll'
+          this.top=0
+        } else if (optionLen*34>distance) {
+          this.top=h-optionLen*34-10
+        }
+      }
+    },
+    handleToggleMenu(flag) {
+      this.isShow = (flag === false) ? false : !this.isShow;
+      if (this.isShow) {
+        this.cascadeTimer=(new Date()).valueOf()
+        this.$store.state.cascadeTimer=this.cascadeTimer
+      }
+    },
+    hideMenu(e) {
       if (this.isShow === false) return;
       this.handleToggleMenu(false);
     },
@@ -46,11 +84,32 @@ export default {
       this.currentOption=Object.assign({},opts)
       this.$emit('input', opts.value);
       this.isShow=false;
-
+    },
+    findCurrentOption(options,val){
+      options.map((elem, index) => {
+        if (val==elem.value) {
+          this.currentOption=Object.assign({},elem)
+        }else if (elem.children) {
+          this.findCurrentOption(elem.children,val)
+        }
+      })
+    }
+  },
+  watch:{
+    value(val){
+      this.findCurrentOption(this.options,val)
+    },
+    '$store.state.cascadeTimer'(val){
+      console.log(val);
+      if (val!=this.cascadeTimer) {
+        this.isShow=false
+      }
+      
     },
   },
   mounted() {
     document.addEventListener('click', this.hideMenu);
+    window.addEventListener('scroll', this.handleScroll, true);
   },
   beforeDestroy() {
     document.removeEventListener('click', this.hideMenu);
@@ -64,15 +123,14 @@ export default {
   position: relative;
   font-size: 14px;
   display: inline-block;
+  background: white;
 
   span {
     display: flex;
     align-items: center;
     width: 100%;
-    height: 34px;
-    line-height: 34px;
-    border-radius: 4px;
-    border: 1px solid #999;
+    height: 100%;
+    border: solid 1px #d2d2d2;
     padding-left: 10px;
     box-sizing: border-box;
     color: #666;
@@ -89,10 +147,11 @@ export default {
       right: 10px;
       transition: 200ms linear;
       transform: rotate(0);
+      z-index: 0;
     }
 
     &.active {
-      border-color: #409EFF;
+      border-color: #ff493f;
 
       .caret {
         transform: rotate(-180deg);
@@ -112,7 +171,7 @@ export default {
   padding: 0px;
   border-radius: 4px;
   color: #666;
-  position: absolute;
+  position: fixed;
   top: 36px;
   left: 0;
   // width: 100%;
@@ -132,7 +191,7 @@ export default {
     border-bottom: 8px solid #eee;
   }
 }
-li {
+.wj-select ul li {
   list-style: none;
   line-height: 30px;
   padding-left: 10px;
